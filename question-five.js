be3d7da7e52204599efe1847e55a9c8fceb7c2d5
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-08-17 23:45:54 +0200

    submitted with two versions

--- question-five.js
+++ question-five.js
@@ -1,12 +1,39 @@
-const str =
+const teststrTrue =
   "ipsum dolor sit amet consectetur adipisicing elit. Error quae nulla voluptatibus ut neque atque expedita cumque illo consequatur aliquid quis porro omnis beatae, culpa aspernatur sequi? Adipisci, quia obcaecati vitae laborum deserunt Mary totam corporis modi nostrum explicabo numquam. Dolor harum suscipit deleniti soluta sint rem quae, at a amet. Explicabo ducimus fugit illo aut praesentium aliquid, ea odit John est quisquam officiis repudiandae consequatur inventore modi eos. Ratione quaerat laudantium tempora accusantium facere necessitatibus, similique velit eius unde beatae ipsam, repellat molestiae, explicabo asperiores sapiente corporis? Architecto, quos quia. Sit eos quia modi harum aut minus architecto illo";
 
-const strFalse =
+const teststrFalse =
   "John dolor sit amet consectetur adipisicing elit. Error quae nulla voluptatibus ut neque atque expedita cumque illo consequatur aliquid quis porro omnis beatae, culpa aspernatur sequi? Adipisci, quia obcaecati vitae laborum deserunt Mary totam corporis modi nostrum explicabo numquam. Dolor harum suscipit deleniti soluta sint rem quae, at a amet. Explicabo ducimus fugit illo aut praesentium aliquid, ea odit John est quisquam officiis repudiandae consequatur inventore modi eos. Ratione quaerat laudantium tempora accusantium facere necessitatibus, similique velit eius unde beatae ipsam, repellat molestiae, explicabo asperiores sapiente corporis? Architecto, quos quia. Sit eos quia modi harum aut minus architecto illo";
 
-function johnMary(str) {
-  const re = /(John|Mary)/;
-  console.log(str.match(re));
+const noWhiteSpace = "Mary&John";
+
+// function johnMary(str) {
+//   const re = /(John|Mary)/g;
+//   const matches = str.match(re);
+//   const johns = matches.filter((el) => el === "John").length;
+//   const marys = matches.filter((el) => el === "Mary").length;
+//   let result;
+//   johns === marys ? (result = true) : (result = false);
+//   return result;
+// }
+
+function johnMary(str, re) {
+  const re = /(John|Mary)/g;
+  const matches = str.match(re);
+  const countedMatches = matches.reduce(function (allNames, name) {
+    if (name in allNames) {
+      allNames[name]++;
+    } else {
+      allNames[name] = 1;
+    }
+    return allNames;
+  }, {});
+  console.log(countedMatches);
+  const result = Object.values(countedMatches).every(
+    (el, i, arr) => el === arr[0]
+  );
+  return result;
 }
 
-console.log(johnMary("John&Mary"));
+console.log(johnMary(noWhiteSpace));
+console.log(johnMary(teststrTrue));
+console.log(johnMary(teststrFalse));
